6c0cc950d13a4fa2a2ccc6536aa7d0aced124bc1
Joaquín Gallego <gallego2@gmail.com>
2015-07-17 10:40:32 +0200

    Creación de preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,12 @@ router.param('quizId', quizController.load);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 router.get('/author', function(req,res,next){
   res.render('author', {nombre:'Joaquín Gallego'});
 });
 
 module.exports = router;
+
